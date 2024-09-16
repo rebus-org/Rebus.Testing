fde6c8070399b5f5826a1b71689cad15d7623dfb
Mogens Heller Grabe <mookid8000@gmail.com>
2024-09-16 20:28:57 +0200

    add saga test example

---
+++ Rebus.TestHelpers.Tests/Bugs/CheckExampleFromBugReport.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Handlers;
+using Rebus.Sagas;
+
+namespace Rebus.TestHelpers.Tests.Bugs;
+
+[TestFixture]
+public class CheckExampleFromBugReport
+{
+    [Test]
+    public void When_TriggeringMySaga_Then_ASagaIsStartedWithCorrectData()
+    {
+        // arrange
+        var correlationId = Guid.NewGuid().ToString();
+        var message = new TriggerMySagaMessage(correlationId);
+
+        // act
+        using var fixture = SagaFixture.For<MySaga>();
+        fixture.Deliver(message);
+
+        var data = fixture.Data.OfType<MySagaData>().First();
+
+        // assert
+        Assert.That(data, Is.Not.Null);
+        Assert.That(data.CorrelationId, Is.EqualTo(correlationId));
+    }
+
+    public class MySaga : Saga<MySagaData>,
+        IAmInitiatedBy<TriggerMySagaMessage>,
+        IHandleMessages<UpdateMySagaMessage>
+    {
+        protected override void CorrelateMessages(ICorrelationConfig<MySagaData> config)
+        {
+            config.Correlate<TriggerMySagaMessage>(m => m.CorrelationId, d => d.CorrelationId);
+        }
+
+        public Task Handle(TriggerMySagaMessage message)
+        {
+            //if (IsNew) Data.CorrelationId = message.CorrelationId;
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(UpdateMySagaMessage message)
+        {
+            Data.CorrelationId = message.CorrelationId;
+            return Task.CompletedTask;
+        }
+    }
+
+    public class TriggerMySagaMessage
+    {
+        [JsonConstructor]
+        public TriggerMySagaMessage(string correlationId, string data)
+        {
+            CorrelationId = correlationId;
+            Data = data;
+        }
+
+        public TriggerMySagaMessage(string correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+
+        public string CorrelationId { get; set; }
+        public string Data { get; }
+    }
+
+    public class UpdateMySagaMessage(string correlationId)
+    {
+        public string CorrelationId { get; set; } = correlationId;
+    }
+
+    public class MySagaData : ISagaData
+    {
+        public Guid Id { get; set; }
+
+        public int Revision { get; set; }
+
+        public string CorrelationId { get; set; }
+    }
+}
